91711e4a3d2046c1565aca670cfbcec3cfafabaf
Raymond Wu <rayw.u17@gmail.com>
2019-09-28 20:37:48 -0400

    Add Sidebar content to App
    Added select state field
    Added select view field
    Added three tabs
    Added content for first two tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Map from './components/Map/LeafletMap';
 import Sidebar from './components/Sidebar/Sidebar';
 import './App.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 function App() {
   return (
     <div className="App flex" >
-      <Sidebar></Sidebar>
+      <Sidebar ></Sidebar>
+      
       <Map></Map>
     </div>
   );
