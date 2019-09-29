f7ce4472a7c3ba8f42be7a3283d684c1ad759427
Sean Chu <SeanChu7@users.noreply.github.com>
2019-09-28 21:51:13 -0400

    Fill the third tab with sliders and inputs
    It adds the play button and the forward and reverse buttons.
    
    It adds Input Box and Sliders for the Number of Congressional Districts
    and Number of Max-Min Districts.
    
    It adds a dropdown menu to select Ethnic Groups.
    
    It adds a range slider to change the Min-Max percentages of the ethnic
    groups.

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -3,6 +3,8 @@ import './Sidebar.css';
 import {Dropdown} from 'primereact/dropdown';
 import {TabView,TabPanel} from 'primereact/tabview';
 import {Fieldset} from 'primereact/fieldset';
+import {InputText} from 'primereact/inputtext';
+import {Slider} from 'primereact/slider';
 const lorum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi neque, ornare at tincidunt id, sollicitudin nec neque. Morbi vitae efficitur dui. Vivamus sodales vitae quam in aliquet. Fusce laoreet eleifend sodales. Nam eleifend lorem quis vehicula imperdiet. Aliquam pulvinar dapibus tellus, et convallis odio suscipit a. Morbi ex orci, dictum at lectus quis, placerat blandit erat. Nunc sit amet tempor leo. Donec vitae arcu a lectus luctus sollicitudin at in mauris. Vivamus ligula ante, auctor sit amet iaculis in, efficitur eu nibh. Suspendisse nec nunc semper, hendrerit nunc fermentum, sagittis nisl. Mauris interdum eros sollicitudin massa aliquet tempus. Praesent mattis fringilla dolor, vitae auctor sem congue vitae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi neque, ornare at tincidunt id, sollicitudin nec neque. Morbi vitae efficitur dui. Vivamus sodales vitae quam in aliquet. Fusce laoreet eleifend sodales. Nam eleifend lorem quis vehicula imperdiet. Aliquam pulvinar dapibus tellus, et convallis odio suscipit a. Morbi ex orci, dictum at lectus quis, placerat blandit erat. Nunc sit amet tempor leo. Donec vitae arcu a lectus luctus sollicitudin at in mauris. Vivamus ligula ante, auctor sit amet iaculis in, efficitur eu nibh. Suspendisse nec nunc semper, hendrerit nunc fermentum, sagittis nisl. Mauris interdum eros sollicitudin massa aliquet tempus. Praesent mattis fringilla dolor, vitae auctor sem congue vitae."
 
 const states=[
@@ -16,7 +18,12 @@ const views=[
     {label:"Voting Precincts", value:"VP"},
     {label:"New Districts", value:"ND"},
 ]
-
+const ethnics= [
+    {label:"American Indian or Alaskan Native", value:"AI"},
+    {label:"Asian", value:"A"},
+    {label:"Black or African American", value:"AA"},
+    {label:"Hawaiian or Pacific Islander", value:"HPI"},
+]
 const elections=[
     {label:"2016 Presidential Election", value: "2016P"},
     {label:"2016 Congressional Election", value: "2016C"},
@@ -29,9 +36,43 @@ class Sidebar extends React.Component{
         this.state = {
             state: null,
             view: null,
+            distNum: null,
+            mDistNum: null,
+            ethnic: "AI",
+            rangeValues:{"AI":[0,100],
+            "A":[0,100],
+            "AA":[0,100],
+            "HPI":[0,100],},
             tab:0,
             election: "2016P",
         }
+    }
+    onChangeSlider2 = (e) => {
+        var newValue;
+        if (e.target && e.target.name === "distNum") {
+            newValue = e.target.value;
+        }
+        else {
+            newValue = e.value;
+        }
+
+        this.setState({ distNum: newValue });
+    }
+    onChangeSlider3 = (e) => {
+        var newValue;
+        if (e.target && e.target.name === "mDistNum") {
+            newValue = e.target.value;
+        }
+        else {
+            newValue = e.value;
+        }
+
+        this.setState({ distNum: newValue });
+    }
+    onChangeRangeSlider = (e) => {
+        var tempList = this.state.rangeValues;
+        tempList[this.state.ethnic] = e.value;
+        this.setState({ rangeValues: tempList });
     }
     render(){
         return(
@@ -69,7 +110,27 @@ class Sidebar extends React.Component{
                         </Fieldset>
                     </TabPanel>
                     <TabPanel contentClassName="content" header={this.state.tab===2?" Redistrict":""} leftIcon="pi pi-caret-right">
-                        Content III
+                        <div className="center play"><i className="pi pi-step-backward" style={{'fontSize':'15px'}}></i>
+                        <i className="pi pi-caret-right" style={{'fontSize':'30px'}}></i>
+                        <i className="pi pi-step-forward" style={{'fontSize':'15px'}}></i></div>
+                        <div className="center">
+                        <div>Number of Districts:</div>
+                        <InputText name="distNum"value={this.state.distNum} style={{width: '100%'}} type="number" onChange={this.onChangeSlider2} />
+                        <Slider name="distNum"value={this.state.distNum} onChange={this.onChangeSlider2} style={{width: '14em'}} />
+                        <div>Number of Maj-min Districts:</div>
+                        <InputText name="mDistNum"value={this.state.mDistNum} style={{width: '100%'}} type="number" onChange={this.onChangeSlider3} />
+                        <Slider name="mDistNum"value={this.state.mDistNum} onChange={this.onChangeSlider3} style={{width: '14em'}} />
+                        <Dropdown placeholder="Select Ethnic Group" value={this.state.ethnic} options={ethnics} onChange={(e) => {this.setState({ethnic: e.value})}}></Dropdown>
+                        <div>Min,Max: {this.state.rangeValues[this.state.ethnic][0]},{this.state.rangeValues[this.state.ethnic][1]}</div>
+                        <Slider value={this.state.rangeValues[this.state.ethnic]} onChange={this.onChangeRangeSlider} range={true} style={{width: '14em'}} />
+                        <Fieldset className="fieldset" legend="Selected Region Statistics">
+                        <div>American Indian or Alaska Native:</div>
+                            <div>Asian:</div>
+                            <div>Black or African American:</div>
+                            <div>Hawaiian or Pacific Islander:</div>
+                            <div>White:</div>
+                        </Fieldset>
+                    </div>
                     </TabPanel>
                 </TabView>
 
