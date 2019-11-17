441d833743b96ca9e33de189fefda3710956f81d
Raymond Wu <rayw.u17@gmail.com>
2019-11-17 17:29:11 -0500

    Will allow for zoom to districts on click

--- frontend/src/components/Map/LeafletMap.js
+++ frontend/src/components/Map/LeafletMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Map, TileLayer, GeoJSON} from 'react-leaflet';
+import {Map, TileLayer, GeoJSON, FeatureGroup} from 'react-leaflet';
 import './Map.css';
 import * as pa from '../../data/penndata.json';
 import * as ri from '../../data/ridata.json';
@@ -7,16 +7,25 @@ import * as ca from '../../data/calidata.json';
 
 const position = [38, -98]
 class LeafletMap extends React.Component{
+
+    constructor(props) {
+      super(props);
+      this.mapRef = React.createRef();
+    }
+    handleClick = (e,element)=>{
+      this.mapRef.current.leafletElement.fitBounds(e.layer.getBounds())
+    }
     render(){
         return(
-          <Map center={position} zoom={5}id="map">
+          <Map center={position} ref={this.mapRef} zoom={5}id="map">
               <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-                <GeoJSON data={pa['default']}></GeoJSON>
-                <GeoJSON data={ri['default']}></GeoJSON>
-                <GeoJSON data={ca['default']}></GeoJSON>
+    />        <FeatureGroup>
+                <GeoJSON data={pa['default']} onClick={(e)=>this.handleClick(e,this)} ></GeoJSON>
+                <GeoJSON data={ri['default']} onClick={(e)=>this.handleClick(e,this)}></GeoJSON>
+                <GeoJSON data={ca['default']} onClick={(e)=>this.handleClick(e,this)}></GeoJSON>
+                </FeatureGroup>
           </Map>
         );
     }
